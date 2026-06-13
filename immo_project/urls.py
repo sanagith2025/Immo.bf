c0c82932640c80c11a4a4a087c462c3bf9cd918f
RAZAKOU SANA <razakousana30@gmail.com>
2026-06-13 16:34:36 +0000

    Ajout page d'accueil temporaire

--- immo_project/urls.py
+++ immo_project/urls.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.http import HttpResponse
+
+# Vue temporaire pour la page d'accueil
+def accueil(request):
+    return HttpResponse("""
+        <h1>Bienvenue sur Immo.bf !</h1>
+        <p>Votre site Django est en ligne avec succès 🎉</p>
+        <hr>
+        <p><a href='/admin/'>Administration</a></p>
+    """)
+
+def health_check(request):
+    return HttpResponse("OK")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('immobilier.urls')),
+    path('', accueil),  # Page d'accueil
+    path('health-check/', health_check),  # Pour Render
+    path('admin/', admin.site.urls),  # Interface admin
+    # path('', include('immobilier.urls')),  # Décommentez quand vos URLs sont prêtes
 ]
-
-# Servir les fichiers média uniquement en développement
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
