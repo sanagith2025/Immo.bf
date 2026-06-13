39b00b9621bab89fdcac6e9db03164d0ec91ea4d
sanagith2025 <razakousana30@gmail.com>
2026-06-13 16:43:06 +0000

    Replace accueil view with home view

--- immo_project/urls.py
+++ immo_project/urls.py
@@ -1,22 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.http import HttpResponse
 
-# Vue temporaire pour la page d'accueil
-def accueil(request):
-    return HttpResponse("""
-        <h1>Bienvenue sur Immo.bf !</h1>
-        <p>Votre site Django est en ligne avec succès 🎉</p>
-        <hr>
-        <p><a href='/admin/'>Administration</a></p>
-    """)
-
-def health_check(request):
-    return HttpResponse("OK")
+def home(request):
+    return HttpResponse("Hello World! Django fonctionne!")
 
 urlpatterns = [
-    path('', accueil),  # Page d'accueil
-    path('health-check/', health_check),  # Pour Render
-    path('admin/', admin.site.urls),  # Interface admin
-    # path('', include('immobilier.urls')),  # Décommentez quand vos URLs sont prêtes
+    path('', home),
+    path('admin/', admin.site.urls),
 ]
